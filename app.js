0959e4eb06ffdf99c34e80b96035b9a1f26155b7
Aadya Singh <68416322+adysis14@users.noreply.github.com>
2023-07-03 14:21:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // app.js
 const moment = require('moment');
 const date = moment().format('LL');
-console.log("Hi, Demo");
+console.log("Hello, Just a Demo!");
 console.log("Current date:", date);
 module.exports = date;
