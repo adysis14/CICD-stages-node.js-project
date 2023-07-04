02fade49699d2ed2c63bb403a8964af407d430e0
Aadya Singh <68416322+adysis14@users.noreply.github.com>
2023-07-04 17:06:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // app.js
 const moment = require('moment');
 const date = moment().format('LL');
+aws_access_key = "AKIASP2TPHJS3AVMLWWW",
 console.log("Hello, Just a Demo!");
 console.log("Current date:", date);
 module.exports = date;
