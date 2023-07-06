4a374ee6c44bfea9885d9810f25bd587c8c33459
Aadya Singh <68416322+adysis14@users.noreply.github.com>
2023-07-06 16:11:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // app.js
 const moment = require('moment');
 const date = moment().format('LL');
+const DiscordClientSecret = "8dyfuiRyqFvVc3RRr_edRk-fZ__JItpP" ;
 aws_access_key = "AKIASP2TPHJS3AVMLWWW",
 aws_access_token = "AKIASP2TPHJS3AVMLYYY"
 console.log("Hello, Just a Demo!");
 console.log("Current date:", date);
 module.exports = date;
 
-const DiscordClientSecret = "8dyfuiRyqFvVc3RRr_edRk-fZ__JItpP" ;
+
+aws_access_token = "AKIASP2TPHJS3AVMLKYY"
